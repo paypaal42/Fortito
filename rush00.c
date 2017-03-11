07e3b306647007583888fee91f936cdd2565820e
Felix WUTSCHKE <fwutschk@e3r12p10.42.fr>
2017-03-11 15:22:11 +0100

    ..

--- rush00.c
+++ rush00.c
@@ -6,32 +6,26 @@
 /*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/11 12:51:44 by pringsta          #+#    #+#             */
-/*   Updated: 2017/03/11 15:00:48 by pringsta         ###   ########.fr       */
+/*   Updated: 2017/03/11 15:21:55 by fwutschk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-
-
 void	ft_charpattern(int a, char typea, char typeb, char typec)
 {
 	int i;
 
-	i=0;
+	i=-1;
 	if(a > 0)
 		putchar(typea);
 	if(a > 2)
-		while(i < (a-2))
-		{
+		while(i++ < (a-2))
 			putchar(typeb);
-			i++;
-		}
 	if(a>1)
 		putchar(typec);
 	putchar('\n');
 }	
-
 void	head(int a)
 {
 	ft_charpattern(a, '/', '*', '\\');
@@ -40,29 +34,22 @@ void	body(int a, int b)
 {	
 	int i;
 
-	i=0;
-	while(i < b-2)
-	{
+	i=-1;
+	while(i++ < b-2)
 		ft_charpattern(a, '|', ' ', '|');
-		i++;
-	}	
 }
 void	tail(int a)
 {	
 	ft_charpattern(a, '\\', '*', '/');
 }
-
 void	rush(int a, int b)
 {
 	head(a);
 	body(a, b);
 	tail(b);
 }
-
-
 int		main()
 {
-	rush(5,5);
+	rush(10,10);
 	return(0);
 }
-
