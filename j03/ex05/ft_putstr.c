9cb41f5504bc4f827fdcbead70c87f42471c6ded
Paal RINGSTAD <pringsta@e3r10p5.42.fr>
2017-03-14 12:31:44 +0100

    reserve day3

---
+++ j03/ex05/ft_putstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/11 21:53:10 by pringsta          #+#    #+#             */
+/*   Updated: 2017/03/14 12:13:26 by pringsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+int		ft_strlength(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (i < ft_strlength(str))
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
