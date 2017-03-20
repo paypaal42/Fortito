522a7c05eacda8fc53bbc6894801ffbb010d2a8b
twinturtle <paalringstad@gmail.com>
2017-03-20 06:44:52 +0100

    fixing exercise 5

--- j05/ex05/ft_strstr.c
+++ j05/ex05/ft_strstr.c
@@ -11,60 +11,29 @@
 /* ************************************************************************** */
 
 #include <string.h>
-#include <stdio.h>
 
-int		compare_str(char c, char d)
+char  *ft_strstr(char *str, char *to_find)
 {
-	return (c == d) ? (1) : (0);
-}
+  int i;
+  int j;
+  int k;
 
-int		str_length(char *to_find)
-{
-	int i;
-
-	i = 0;
-	while (to_find[i] != '\0')
-		i++;
-	return (i);
-}
-
-
-char	*ft_strstr(char *str, char *to_find)
-{
-	int i1 = 0;
-	int n2 = -1;
-	int v3 = 0;
-	int n_len = 0;
-	char output[10];
-
-	while (str[i1] != '\0')
-	{
-		v3 = i1;
-		if (compare_str(str[v3], to_find[n_len]))	
-		{
-			while (n2++ < str_length(to_find) && compare_str(str[v3], to_find[n_len]))
-			{  	
-				v3++;
-				n_len++;
-			}
-			n_len = -1;
-			if (n2 == str_length(to_find))
-			{
-				while (str[i1] != '\0')
-				{
-					output[n_len++] = str[i1];
-					i1++;
-				}
-			}
-			else 
-				v3 = 0;
-		}
-	i1++;
-	}
-	return (output);
-}
-
-int		main(void)
-{
-	return(0);
+  i = 0;
+  j = 0;
+  k = 0;
+  while (str[i])
+  {
+    while (str[i] == to_find[j])
+    {
+      if (to_find[j + 1] == '\0')
+        return (str + i - j);
+      i++;
+      j++;
+    }
+    j = 0;
+    i = k;
+    i++;
+    k++;
+  }
+  return (NULL);
 }
