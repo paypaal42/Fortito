77a65fd4656381082c09cac8c16fa0fe5c7021b0
twinturtle <paalringstad@gmail.com>
2017-03-29 22:54:02 +0200

    whitespaces

--- j08/ex00/ft_split_whitespaces.c
+++ j08/ex00/ft_split_whitespaces.c
@@ -1,71 +1,70 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_split_whitespaces.c                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/29 11:55:43 by pringsta          #+#    #+#             */
-/*   Updated: 2017/03/29 15:24:51 by pringsta         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdlib.h>
 
-int		ft_c_l(char *str)
+int			ft_count_words(char *str)
 {
-	int i;
+	int	i;
+	int	k;
 
 	i = 0;
-	while (str[i] && str[i] != 8 && str[i] != 9 && str[i] != 32)
+	k = 0;
+	while (str[i])
+	{
+		while ((str[i] == ' ' || str[i] == '\t' || str[i] == '\n') && str[i])
+			i++;
+		while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i])
+			i++;
+		k++;
+	}
+	if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n')
+		k--;
+	return (k);
+}
+
+int			ft_count_characters(char *str, int i)
+{
+	int		chars;
+
+	chars = 0;
+	while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i])
+	{
+		chars++;
+		i++;
+	}
+	return (chars);
+}
+
+int			ft_next_word_index(char *str, int i)
+{
+	while ((str[i] == ' ' || str[i] == '\t' || str[i] == '\n') && str[i])
 		i++;
 	return (i);
 }
 
-int		ft_nb_words(char *str)
-{
-	int i;
-	int w_c;
-
-	i = 0;
-	w_c = 0;
-	while (str[i])
-	{
-		while (str[i] && (str[i] == 8 || str[i] == 9 || str[i] == 32))
-			i++;
-		if (str[i])
-			w_c++;
-		while (str[i] && str[i] != 8 && str[i] != 9 && str[i] != 32)
-			i++;
-	}
-	return (w_c);
-}
-
-char	**ft_split_whitespaces(char *str)
+char		**ft_split_whitespaces(char *str)
 {
 	int		i;
-	int		w_c;
-	int		l;
-	int		w_len;
+	int		j;
+	int		x;
+	int		chars;
 	char	**tab;
 
 	i = 0;
-	w_c = 0;
-	if (!(tab = (char**)malloc(sizeof(char*) * (ft_nb_words(str) + 1))))
-		return (0);
+	j = 0;
+	tab = (char**)(malloc(sizeof(char*) * (ft_count_words(str) + 1)));
 	while (str[i])
 	{
-		while (str[i] && (str[i] == 8 || str[i] == '\n' || str[i] == 32))
+		x = -1;
+		i = ft_next_word_index(str, i);
+		chars = ft_count_characters(str, i);
+		tab[j] = (char*)(malloc(sizeof(char) * (chars) + 1));
+		while (++x < chars)
+		{
+			tab[j][x] = str[i];
 			i++;
-		if (!(tab[w_c] = (char*)malloc(sizeof(char) * (ft_c_l(str + i) + 1))))
-			return (0);
-		w_len = ft_c_l(str + i);
-		l = 0;
-		while (l < w_len)
-			tab[w_c][l++] = str[i++];
-		if (w_len != 0)
-			tab[w_c++][l] = '\0';
+		}
+		tab[j][x] = '\0';
+		j++;
 	}
-	tab[w_c] = 0;
-	return (tab);
+	tab[j] = 0;
+	return (chars == 0 ? 0 : tab);
 }
