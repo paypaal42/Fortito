5f13e670596453f6973df8ab7404870647d666f5
Paal RINGSTAD <pringsta@e1r3p1.42.fr>
2017-04-13 18:38:29 +0200

    finishing..

--- LIBFT/1main.c
+++ LIBFT/1main.c
@@ -1,16 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/13 12:51:15 by pringsta          #+#    #+#             */
-/*   Updated: 2017/04/13 12:52:22 by pringsta         ###   ########.fr       */
+/*   Created: 2017/04/13 11:31:20 by pringsta          #+#    #+#             */
+/*   Updated: 2017/04/13 18:37:28 by pringsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	bzero(void *s, size_t n)
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
 
+int	main(int ac, char **av)
+{
+	int i;
+	(void)ac;
+	unsigned char str[20] = "IIIIIIIIIIIIIIIIIII";
+	
+	i = 0;
+	printf("FT_PUTCAHR |--------------------------------\n\n");	
+	printf("\nFT_MEMSET |--> %s\n", ft_memset(str, 97, 5));
+	printf("BZERO |--> %s\n", bzero(str, 10));
+	return (0);
 }
--- LIBFT/libft.h
+++ LIBFT/libft.h
@@ -6,15 +6,18 @@
 /*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 12:07:13 by pringsta          #+#    #+#             */
-/*   Updated: 2017/04/13 12:38:34 by pringsta         ###   ########.fr       */
+/*   Updated: 2017/04/13 15:44:17 by pringsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <string.h>
+# include <unistd.h>
 
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_putchar(char c);
+void    ft_bzero(void *s, size_t n);
 
 typedef struct s_list
 {
