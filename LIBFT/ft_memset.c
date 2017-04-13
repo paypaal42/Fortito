96b0fc3ed0f38d765ecfce930847ed5bff52e39f
Paal RINGSTAD <pringsta@e1r12p6.42.fr>
2017-04-13 11:17:32 +0200

    new function ft_mem

--- LIBFT/ft_memset.c
+++ LIBFT/ft_memset.c
@@ -1,11 +1,35 @@
-#include "libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/13 10:33:20 by pringsta          #+#    #+#             */
+/*   Updated: 2017/04/13 11:16:40 by pringsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void  *ft_memset(void *str, int c, size_t len)
+#include <string.h>
+#include <stdio.h>
+
+void	*ft_memset(void *b, int c, size_t len)
 {
-  unsigned char *dest;
+	unsigned char 	*str;
+	unsigned char	character;
 
-  dest = (unsigned char*)str;
-  while (len-- > 0)
-    *dest++ = (unsigned char)c;
-  return (str);
+	str = (unsigned char*)b;
+	character = c;
+	while (len-- > 0)
+		*str++ = character;
+	return (b);
+}
+
+int	main(void)
+{
+	char paal[10] = "OOOOOOOO";
+	char paal1[10] = "OOOOOOOO";
+	printf("%s\n", memset(paal, 97, 3));
+	printf("%s\n", ft_memset(paal1, 97, 3));
+	return (0);
 }
