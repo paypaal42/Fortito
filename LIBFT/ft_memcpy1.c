2357eee2f91b58470055d3052e9be61daa43e72c
Paal RINGSTAD <pringsta@e2r1p1.42.fr>
2017-04-15 11:57:03 +0200

    ..

--- LIBFT/ft_memcpy1.c
+++ LIBFT/ft_memcpy1.c
@@ -6,18 +6,28 @@
 /*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 16:38:28 by pringsta          #+#    #+#             */
-/*   Updated: 2017/04/15 11:43:03 by pringsta         ###   ########.fr       */
+/*   Updated: 2017/04/15 11:56:40 by pringsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-void			*ft_memcpy(void *restrict dst,
+void			*ft_memcpy1(void *restrict dst,
 				const void *restrict src, size_t n)
 {
 	const char	*src_str;
 	char		*dst_str;
+	size_t 		a;
+	size_t 		b;
+	size_t 		c;
+	a = sizeof(src) ;
+	b = *(&src + 1) - src ;
+	c = sizeof(src) / sizeof(char);
+	printf("sizeof src-------> %zu\n", a);
+	printf("sizeof src-------> %zu\n", b);
+	printf("sizeof src-------> %zu\n", c);
+	printf("adresse src------> %p\n", src);
 	src_str = src;
 	dst_str = dst;
 	while (n > 0)
--- LIBFT/libft.h
+++ LIBFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 12:07:13 by pringsta          #+#    #+#             */
-/*   Updated: 2017/04/14 18:15:04 by pringsta         ###   ########.fr       */
+/*   Updated: 2017/04/15 11:46:06 by pringsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void				*ft_memcpy1(void *restrict dst, const void *restrict src, size_t n);
 void				ft_putchar(char c);
 size_t				ft_strlen(const char *s);
 char				*ft_strdup(const char *s1);
