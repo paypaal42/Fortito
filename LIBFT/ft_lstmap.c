96f6496aa7fa5bc3eb4753bf7adaa8030741cc6e
Paal RINGSTAD <pringsta@e2r7p6.42.fr>
2017-05-05 10:17:36 +0200

    ending bonus part 2

--- LIBFT/ft_lstmap.c
+++ LIBFT/ft_lstmap.c
@@ -6,24 +6,37 @@
 /*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 15:42:14 by pringsta          #+#    #+#             */
-/*   Updated: 2017/05/04 19:20:29 by pringsta         ###   ########.fr       */
+/*   Updated: 2017/05/05 09:59:02 by pringsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-t_list	*ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem))
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list *head;
-	t_list *tmp;
-	if (!(lst || f))
+	t_list	*first;
+	t_list	*head;
+	t_list	*tmp;
+
+	if (!(lst) || !(f))
 		return (0);
-	head = lst;
 	tmp = NULL;
-	if (!(lst || f))
-		return (0);
-	while (head)
+	first = NULL;
+	while (lst)
 	{
-		if (!(ft_lstnew(head->content, head->content_size)))
+		if (!(head = f(lst)))
 			return (0);
-		tmp->next;
-	}	
+		if (first)
+		{
+			tmp->next = head;
+			tmp = tmp->next;
+		}
+		else
+		{
+			first = head;
+			tmp = first;
+		}
+		lst = lst->next;
+	}
+	return (first);
 }
--- LIBFT/libft.h
+++ LIBFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 12:07:13 by pringsta          #+#    #+#             */
-/*   Updated: 2017/05/04 18:20:14 by pringsta         ###   ########.fr       */
+/*   Updated: 2017/05/05 08:39:49 by pringsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,6 @@ void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-void				ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 #endif
