5eff0b5c83783c2b5a4fbd61e8fbbed269defcc8
Paal RINGSTAD <pringsta@e2r10p4.42.fr>
2017-05-11 16:21:05 +0200

    libft finished

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/14 15:42:30 by pringsta          #+#    #+#             */
-/*   Updated: 2017/04/19 18:32:48 by pringsta         ###   ########.fr       */
+/*   Created: 2017/04/20 15:24:10 by pringsta          #+#    #+#             */
+/*   Updated: 2017/05/09 14:11:34 by pringsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "string.h"
+#include "libft.h"
 
-char	*ft_strncpy(char *dst, const char *src, size_t len)
+int	ft_isascii(int c)
 {
-	unsigned int i;
-
-	i = 0;
-	while (i < len && src[i])
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	while (i < len)
-		dst[i++] = '\0';
-	return (dst);
+	return (c >= 0 && c <= 127);
 }
--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -1,16 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pringsta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/20 15:24:10 by pringsta          #+#    #+#             */
-/*   Updated: 2017/04/20 17:02:52 by pringsta         ###   ########.fr       */
+/*   Created: 2017/04/14 15:42:30 by pringsta          #+#    #+#             */
+/*   Updated: 2017/05/09 14:13:14 by pringsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isascii(int c)
+#include "libft.h"
+
+char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	return (c >= 0 && c <= 127);
+	unsigned int i;
+
+	i = 0;
+	while (i < len && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	while (i < len)
+		dst[i++] = '\0';
+	return (dst);
 }
